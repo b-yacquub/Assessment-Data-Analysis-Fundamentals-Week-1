2e414edb9f24baeb98e94f945fa470f47b41f7d9
Yacquub <b_yacquub@hotmail.co.uk>
2025-08-08 15:00:00 +0100

    did c8 more than halfway

--- queries/challenge_7.sql
+++ queries/challenge_7.sql
@@ -2,3 +2,13 @@
 
 -- The column should be called count_of_multiple_orders
 
+with order_more_than_1 as (
+select (order_id) as num_orders
+from orders o 
+join order_details od using(order_id)
+join products p using(product_id)
+group by order_id
+having count(order_id)>1
+)
+select count(*) as count_of_multiple_orders
+from order_more_than_1;
--- queries/challenge_8.sql
+++ queries/challenge_8.sql
@@ -8,3 +8,11 @@
 
 -- When calculating the order price, ignore any discounts and use the warehouse-standard price for the products only
 
+select order_id, sum(p.unit_price )::int as total_price
+from orders o 
+join order_details od using(order_id)
+join products p using(product_id)
+group by o.order_id 
+having sum(p.unit_price )>max(p.unit_price )
+order by o.order_id desc
+limit 10;
