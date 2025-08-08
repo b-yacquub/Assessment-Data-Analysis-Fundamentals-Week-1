1b10f225193d295ca5c0b861fb351a6ec8692f60
Yacquub <b_yacquub@hotmail.co.uk>
2025-08-08 15:06:42 +0100

    did c9

--- queries/challenge_9.sql
+++ queries/challenge_9.sql
@@ -2,3 +2,7 @@
 
 -- Results should be ordered in descending order of employment year, with ties broken by sorting alphabetically by last name (ascending).
 
+select (title_of_courtesy ||' '||first_name ||' '||last_name ) 
+as employee_full_name 
+from employees
+order by hire_date desc;
