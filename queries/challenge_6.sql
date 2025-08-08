8352e197d9f7b30579e2d0d9d5ffe21b2f832d06
Yacquub <b_yacquub@hotmail.co.uk>
2025-08-08 14:36:32 +0100

    did c6

--- queries/challenge_6.sql
+++ queries/challenge_6.sql
@@ -6,3 +6,9 @@
 
 -- Results should be sorted alphabetically by supplier and category.
 
+select company_name as supplier,category_name as category, avg(p.unit_price ) over (partition by category_name) as average_price
+from products p 
+join categories using(category_id)
+join suppliers using(supplier_id)
+where category_name ilike '%m%'
+order by category_name desc;
