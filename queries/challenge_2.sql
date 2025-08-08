f85f3cd3d50198db5257a5e2c0d6d86b3b3fb12b
Yacquub <b_yacquub@hotmail.co.uk>
2025-08-08 14:12:40 +0100

    got 3 passing

--- queries/challenge_2.sql
+++ queries/challenge_2.sql
@@ -1,2 +1,3 @@
 -- A query that displays the total integer value of all products in stock as "stock_value".
 
+select sum(units_in_stock) as stock_value from products;
--- queries/challenge_3.sql
+++ queries/challenge_3.sql
@@ -2,3 +2,8 @@
 
 -- Results should be ordered alphabetically by category_name and product_name
 
+select category_name, product_name
+from products
+join categories using(category_id)
+where product_name ilike 's%'
+order by product_name desc;
